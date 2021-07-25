795b0bd59617f83c2cfcbe2c531b9e4fc5d60995
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-07-26 00:37:22 +1000

    Add Content and Style in Header Components.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,39 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{display:"flex", alignItems:"center", color:"white", marginBottom:"15px",}}>
+          <DiCssdeck size="3rem" /><Span>Portofolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <Link href="#projects">
+        <NavLink>projects</NavLink>
+      </Link>
+      <Link href="#Skill">
+        <NavLink>Skill</NavLink>
+      </Link>
+      <Link href="#About">
+        <NavLink>About</NavLink>
+      </Link>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com" target="_blank">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com" target="_blank">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
